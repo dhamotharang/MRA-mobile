b36d10fd4116404bd88e6a26cfcdd00f54de2d99
nisahasin <nisahasin95@gmail.com>
2021-03-02 17:14:36 +0800

    remove ios platform and add plugin for android....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,26 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CallNumber } from '@ionic-native/call-number/ngx';
+import { LaunchNavigator } from '@ionic-native/launch-navigator/ngx';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    BarcodeScanner,
+    CallNumber,
+    LaunchNavigator,
+    Camera
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
